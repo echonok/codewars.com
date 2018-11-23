b5e55899bbf2138b51265f4a62c30e8b3de4d5ab
Nick Elovsky <echonok@gmail.com>
2018-11-23 17:05:03 +0300

    add

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function wave(someString){
-  const stringLength = someString.length;
+  const stringLength = someString.replace(/\s/g,'').length;
   
-  const newArr = Array.apply(null, Array(5)).map(function () {});
-  console.log(newArr);
-
-  const finalArr = newArr.map((elem, index) => capitalezeLetter(someString, index + 1));
-  console.log(finalArr);
+  console.log(stringLength);
+  const newArr = Array.apply(null, Array(stringLength)).map(function () {});
+  
+  const finalArr = newArr.map((elem, index) => {
+      //if (someString.substring(index, index) !== ' ') {
+        return capitalazeLetter(someString, index + 1);
+      //}      
+    });
+  
+  return finalArr;
 }
 
-function capitalezeLetter(text, index){
+function capitalazeLetter(text, index){
   const newText = text.substring(0, index - 1) + text.substring(index - 1, index).toUpperCase() + text.substring(index, text.length) ;
   return newText;
 }
