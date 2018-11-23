8434abd0d081f41b6aff034f8bb9e728f7325cb5
Nick Elovsky <echonok@gmail.com>
2018-11-23 16:33:26 +0300

    add some code

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function wave(){
-  // Code here
+function wave(someString){
+  const stringLength = someString.length;
+  
+  const newArr = Array.apply(null, Array(5)).map(function () {});
+  console.log(newArr);
+
+  const finalArr = newArr.map((elem, index) => capitalezeLetter(someString, index + 1));
+  console.log(finalArr);
+}
+
+function capitalezeLetter(text, index){
+  const newText = text.substring(0, index - 1) + text.substring(index - 1, index).toUpperCase() + text.substring(index, text.length) ;
+  return newText;
 }
